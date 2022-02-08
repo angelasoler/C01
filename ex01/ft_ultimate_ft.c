ddc85d5e77fd6952135dcb236f384f8cf84dc101
asoler <asoler@student.42sp.org.br>
2022-02-08 01:54:03 +0100

    pointer of pointer

--- ex01/ft_ultimate_ft.c
+++ ex01/ft_ultimate_ft.c
@@ -6,7 +6,7 @@
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 19:23:17 by asoler            #+#    #+#             */
-/*   Updated: 2022/02/07 19:59:35 by asoler           ###   ########.fr       */
+/*   Updated: 2022/02/08 01:51:35 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,28 +19,27 @@ void	ft_ft(int *********nbr)
 
 int	main(void)
 {
-	int *nbr;
-	int **nbr;
-	int ***nbr;
-	int ****nbr;
-	int *****nbr;
-	int ******nbr;
-	int *******nbr;
-	int ********nbr;
-	int *********nbr;
+	int n;
+	int *n1;
+	int **n2;
+	int ***n3;
+	int ****n4;
+	int *****n5;
+	int ******n6;
+	int *******n7;
+	int ********n8;
 	
+	n = 15;
+	n1 = &n;
+	n2 = &n1;
+	n3 = &n2;
+	n4 = &n3;
+	n5 = &n4;
+	n6 = &n5;
+	n7 = &n6;
+	n8 = &n7;
 	
-
-	**nbr = *nbr;
-	***nbr = ****nbr;
-	****nbr = *****nbr;
-	*****nbr = ******nbr;
-	******nbr = *******nbr;
-	*******nbr = ********nbr;
-	********nbr = *********nbr;
-	*********nbr = 15;
-	
-	printf("%d", *********nbr);
-	ft_ft(*********nbr);
-	printf("%d", *********nbr);
+	printf("%d", n);
+	ft_ft(&n8);
+	printf("%d", n);
 }
